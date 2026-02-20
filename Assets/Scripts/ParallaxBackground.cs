b374a6f0937f8aa21a8536761c335058fd85ec43
dlwjdals102 <35514847+dlwjdals102@users.noreply.github.com>
2026-02-20 21:08:59 +0900

    타일맵, 시네머신, background 추가
    타일맵 ( +타일팔레트 ) 추가
    성능향상을 위해 composite colider 사용, 움직임 향상을 위해 geometry type = polygons로 변경,
    
    카메라 -> 시네머신 적용 (플레이어 추적 및 카메라로 볼 수 있는 맵 제한)
    
    배경추가
    배경이 카메라를 따라다니게 구현
    이동 시 약간의 떨림 발생 > float 계산을 하다보니 약간의 차이 발생 > fixedupdate로 해결

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,74 @@
+using System;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    private Camera mainCamera;
+    private float lastCameraPositionX;
+    private float cameraHalfWidth;
+
+    [SerializeField] private ParallaxLayer[] backgroundLayer;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        cameraHalfWidth = mainCamera.orthographicSize * mainCamera.aspect;
+        CalculateImageLength();
+    }
+
+    // 배경 떨림을 방지하기 위한 FixedUpdate
+    private void FixedUpdate()
+    {
+        float currentCameraPositionX = mainCamera.transform.position.x;
+        float distanceToMove = currentCameraPositionX - lastCameraPositionX;
+        lastCameraPositionX = currentCameraPositionX;
+
+        float cameraLeftEdge = currentCameraPositionX - cameraHalfWidth;
+        float cameraRightEdge = currentCameraPositionX + cameraHalfWidth;
+
+        foreach (var layer in backgroundLayer)
+        {
+            layer.Move(distanceToMove);
+            layer.LoopBackground(cameraLeftEdge, cameraRightEdge);
+        }
+    }
+
+    private void CalculateImageLength()
+    {
+        foreach (var layer in backgroundLayer)
+            layer.CalculateImageWidth();
+    }
+}
+
+[Serializable]
+public class ParallaxLayer
+{
+    [SerializeField] private Transform background;
+    [SerializeField] private float parallaxMultiplier;
+    [SerializeField] private float imageWidthOffset = 10; // 카메라가 가장자리에 도착했을 때 약간의 차이가 있음, offset 위치 설정으로 해결
+
+    private float imageFullWidth;
+    private float imageHalfWidth;
+
+    public void CalculateImageWidth()
+    {
+        imageFullWidth = background.GetComponent<SpriteRenderer>().bounds.size.x;
+        imageHalfWidth = imageFullWidth / 2;
+    }
+
+    public void Move(float distanceToMove)
+    {
+        background.position += Vector3.right * (distanceToMove * parallaxMultiplier);
+    }
+
+    public void LoopBackground(float cameraLeftEdge, float cameraRightEdge)
+    {
+        float imageLeftEdge = (background.position.x - imageHalfWidth) + imageWidthOffset;
+        float imageRightEdge = (background.position.x + imageHalfWidth) - imageWidthOffset;
+
+        if (imageLeftEdge > cameraRightEdge)
+            background.position += Vector3.right * -imageFullWidth;
+        else if (imageRightEdge < cameraLeftEdge)
+            background.position += Vector3.right * imageFullWidth;
+    }
+}
