a612e5ea000cd006448f650b23a72f65e8bd4c2a
dlwjdals102 <35514847+dlwjdals102@users.noreply.github.com>
2026-02-21 20:43:42 +0900

    combat, health 추가
    entity에 기능이 너무 많아질것을 우려하여 combat, health 스크립트는 따로 뺏음
    애니메이션 이벤트를 이용해서 공격했을때 상대 정보 얻어옴

---
+++ Assets/Scripts/Entity_Combat.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Entity_Combat : MonoBehaviour
+{
+    public float damage = 10;
+
+    [Header("Target detection")]
+    [SerializeField] private Transform targetCheck;
+    [SerializeField] private float targetCheckRadius;
+    [SerializeField] private LayerMask whatIsTarget;
+
+    public void PerformAttack()
+    {
+        foreach(var target in GetDetectedColliders())
+        {
+            Entity_Health targetHealth = target.GetComponent<Entity_Health>();
+
+            if (targetHealth != null)
+                targetHealth.TakeDamage(damage, transform);
+        }
+    }
+
+    private Collider2D[] GetDetectedColliders()
+    {
+        return Physics2D.OverlapCircleAll(targetCheck.position, targetCheckRadius, whatIsTarget);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(targetCheck.position, targetCheckRadius);
+    }
+}
